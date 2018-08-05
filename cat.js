6214145868212efa45e21b9464f55437375ffe1d
caoviet <vietanhcao1994@gmail.com>
2018-08-05 09:35:34 +0700

    create cat.js

---
+++ cat.js
@@ -0,0 +1,19 @@
+let Mouse = require('./mouse.js');
+
+function Cat(name,age) {
+	this.name = name;
+	this.age = age;
+	this.stomach = [];
+}
+Cat.prototype.eat = function(mouse){
+	this.stomach.push(mouse);
+
+};
+let tom = new Cat(tom,2);
+
+//
+
+tom.eat(Mouse);
+
+
+module.
